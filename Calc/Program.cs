c508a567bf9c91a625e3b8c3b847e78a89b83d8c
Desiderium0 <101682131+Desiderium0@users.noreply.github.com>
2022-03-16 17:54:57 +0800

    Добавьте файлы проекта.

---
+++ Calc/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, next;
+            double a, b, Sum;
+            Console.WriteLine("--------------------------Calculator--------------------------");
+            Console.WriteLine("        '*' -  умножение ");
+            Console.WriteLine("        '/' -  деление ");
+            Console.WriteLine("        '-' -  вычетание ");
+            Console.WriteLine("        '+' -  сложение ");
+            Console.Write("Выберете один из следущих знаков для подсчета: ");
+            n = Convert.ToChar(Console.ReadLine());
+            Console.Write("Введите первое число: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            switch (n)
+            {
+                case '*':
+                    Sum = a * b;
+                    Console.WriteLine("Ваш ответ: {0}", Sum); break;
+                case '/':
+                    Sum = a / b;
+                    Console.WriteLine("Ваш ответ: {0}", Sum); break;
+                case '-':
+                    Sum = a - b;
+                    Console.WriteLine("Ваш ответ: {0}", Sum); break;
+                case '+':
+                    Sum = a + b;
+                    Console.WriteLine("Ваш ответ: {0}", Sum); break;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Хотите вычислить еще что-то?");
+
+            //Цикличность действия
+            Console.Write("Если да нажмите 'Y' для отмены нажмите любую клавишу: ");
+            next = Convert.ToChar(Console.ReadLine());
+            while (next == 'Y' | next == 'y' | next == 'у')
+            {
+
+                Console.WriteLine("--------------------------Calculator--------------------------");
+                Console.WriteLine("        '*' -  умножение ");
+                Console.WriteLine("        '/' -  деление ");
+                Console.WriteLine("        '-' -  вычетание ");
+                Console.WriteLine("        '+' -  сложение ");
+                Console.Write("Выберете один из следущих знаков для подсчета: ");
+                n = Convert.ToChar(Console.ReadLine());
+                Console.Write("Введите первое число: ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                b = Convert.ToDouble(Console.ReadLine());
+                switch (n)
+                {
+                    case '*':
+                        Sum = a * b;
+                        Console.WriteLine("Ваш ответ: {0}", Sum); break;
+                    case '/':
+                        Sum = a / b;
+                        Console.WriteLine("Ваш ответ: {0}", Sum); break;
+                    case '-':
+                        Sum = a - b;
+                        Console.WriteLine("Ваш ответ: {0}", Sum); break;
+                    case '+':
+                        Sum = a + b;
+                        Console.WriteLine("Ваш ответ: {0}", Sum); break;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Хотите вычислить еще что-то?");
+                Console.Write("Если да нажмите 'Y' для отмены нажмите любую клавишу: ");
+                next = Convert.ToChar(Console.ReadLine());
+            }
+            Console.ReadKey();
+        }
+    }
+}
